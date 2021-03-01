b8bc694fc360776d9778d334f01ee71401cf25f4
Ally <el.ally741@gmail.com>
2021-03-01 18:40:18 +0200

    this gone be wonderful

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>This gone be wonderful</h1>
+    </div>
+  );
+}
+
+export default App;
